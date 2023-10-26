04589c896600cb10daab0a77fa23f941a2fbb7e2
Guy David <gaidavid1@gmail.com>
2023-10-26 03:02:48 +0300

    Update logger.py
    there will be only two log files for each service

--- src/logger/logger.py
+++ src/logger/logger.py
@@ -14,7 +14,7 @@ def setup_logger(service_name):
     logger.setLevel(logging.INFO)
 
     # Set up rotating file handler for log file
-    file_handler = RotatingFileHandler(f"/data/data/com.termux/files/home/project-root-directory/cpps-server/logs/{log_file}", maxBytes=5*1024*1024, backupCount=3)
+    file_handler = RotatingFileHandler(f"/data/data/com.termux/files/home/project-root-directory/cpps-server/logs/{log_file}", maxBytes=5*1024*1024, backupCount=1)
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
