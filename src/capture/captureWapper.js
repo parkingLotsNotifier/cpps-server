2c27203c1daba92ecadae7a78cae9fbf0b9377f1
parkingLotsNotifier <parkinglot2024@gmail.com>
2023-11-05 14:44:49 +0200

    device model recognition commented-out

--- src/capture/captureWapper.js
+++ src/capture/captureWapper.js
@@ -8,15 +8,15 @@ const logger = createLogger('captureWapper');
 const capturePhoto = () => {
   return new Promise((resolve, reject) => {
     // Get device model
-    exec('getprop | grep -F ro.product.model | cut -c21-', (error, stdout) => {
-      if (error) {
-        logger.error(`Failed to get device model: ${error.message}`);
-        reject(error);
-        return;
-      }
+    // exec('getprop | grep -F ro.product.model | cut -c21-', (error, stdout) => {
+    //   if (error) {
+    //     logger.error(`Failed to get device model: ${error.message}`);
+    //     reject(error);
+    //     return;
+    //   }
     
 
-      const device_model = stdout.trim();
+      const device_model = '[Prototype]';
       const file_name = `picture-${device_model}-${new Date().toLocaleString('he-IL',{ hour12: false }).replace(/, /g, '-')}`;
       logger.verbose(`Device Model: ${device_model}, File Name: ${file_name}`);
 
@@ -32,7 +32,7 @@ const capturePhoto = () => {
         resolve(file_name);
       });
     });
-  });
+  // });
 };
 
 module.exports = {
