35ac1da8503e6f1feb7002f8cea901c94ebd4f02
parkingLotsNotifier <parkinglot2024@gmail.com>
2023-11-07 17:23:33 +0200

    added TODOS on cors,onPipelineError

--- app.js
+++ app.js
@@ -10,14 +10,15 @@ const { startCPPS } = require('./src/orchestra-conductor/startCPPS');
 database.connect(DB_USERNAME, DB_PASSWORD, DB_HOST).then(() => {
    
     const app = express();
-    app.use(cors());
+    
+    app.use(cors()); //TODO: used for server when deploy in Cross-origin resource sharing
     app.use(bodyParser.json());
     
     
     startCPPS();
 
     onPipelineFinished(startCPPS);
-    onPipelineError(startCPPS)
+    onPipelineError(startCPPS);//TODO: on error just starts again? on complex error it dosent seems right exeptions class
 
     app.listen(PORT, () => console.log(`example app listening on port ${PORT} ${rest.GREETING}`));
 });
